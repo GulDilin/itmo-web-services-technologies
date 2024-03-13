e75d6b0294477da0a7ca5f3551f22a4b19f12400
GulDilin <zhenyagurin@gmail.com>
2024-03-13 12:18:22 +0300

    little change migrations

--- lab1/lab1-server/src/main/resources/db/migration/V1_2__insert_data.sql
+++ lab1/lab1-server/src/main/resources/db/migration/V1_2__insert_data.sql
@@ -1,20 +1,20 @@
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Москва', 77, 18800000, 120, 5115, 77);
+    VALUES ('Moscow', 77, 18800000, 120, 5115, 77);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Санкт-Петербург', 78, 7000000, 200, 3991, 198);
+    VALUES ('Saint-Petersburg', 78, 7000000, 200, 3991, 198);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Ленинградская область', 78, 2023767, 100, 22, 47);
+    VALUES ('Leningrad region', 78, 2023767, 100, 22, 47);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Рязань', 62, 523203, 150, 2406, 62);
+    VALUES ('Ryazan', 62, 523203, 150, 2406, 62);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Владивосток', 25, 597237, 8, 1803, 25);
+    VALUES ('Vladivostok', 25, 597237, 8, 1803, 25);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Краснодар', 23, 1121291, 25, 3802, 23);
+    VALUES ('Krasnodar', 23, 1121291, 25, 3802, 23);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Калининград', 39, 489735, 8, 2179, 39);
+    VALUES ('Kaliningrad', 39, 489735, 8, 2179, 39);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Калининградская область', 39, 1032343, 8, 68, 39);
+    VALUES ('Kaliningrad region', 39, 1032343, 8, 68, 39);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Калуга', 40, 333954, 190, 1978, 40);
+    VALUES ('Kaluga', 40, 333954, 190, 1978, 40);
 INSERT INTO city (name, area, population, meters_above_sea_level, population_density, car_code)
-    VALUES ('Липецк', 48, 490428, 330, 1485, 48);
+    VALUES ('Lipetsk', 48, 490428, 330, 1485, 48);
